ec1b582d1f39d227c46504501f966d147519557c
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-20 17:40:28 +0900

    デコレーションコンポーネント作成

---
+++ src/components/Decoration.tsx
@@ -0,0 +1,10 @@
+export default function Decoration(){
+    return(
+        <>
+            <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute bottom-[-30] left-[-30]"></div>
+            <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute bottom-[150] left-[5]"></div>
+            <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute top-[150] right-[5]"></div>
+            <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute top-[-30] right-[-30]"></div>
+        </>
+    )
+}
