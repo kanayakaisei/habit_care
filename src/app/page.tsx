d2bf5b8b4f09e65fbd376b154ba0f893d7f0caf4
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-20 17:41:00 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Decoration from "@/components/Decoration";
 
 export default function Login() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -27,11 +28,7 @@ export default function Login() {
             <Image src="logo.svg" alt="ハビットケアロゴ" width={245} height={245} />
           </h1>
           <Image src="logo_text.svg" alt="ハビットケア" width={240} height={30} />
-          {/* 装飾 */}
-          <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute bottom-[-30] left-[-30]"></div>
-          <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute bottom-[150] left-[5]"></div>
-          <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute top-[150] right-[5]"></div>
-          <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute top-[-30] right-[-30]"></div>
+          <Decoration />
         </>
       ) : (
         <>
@@ -82,11 +79,7 @@ export default function Login() {
             </Link>
             </form>
 
-          {/* 装飾 */}
-          <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute bottom-[-30] left-[-30]"></div>
-          <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute bottom-[150] left-[5]"></div>
-          <div className="bg-[#44B7D9] w-13 h-13 rounded-full absolute top-[150] right-[5]"></div>
-          <div className="bg-gradient-to-tr from-[#44B7D9] to-[#C0E06B] w-40 h-40 rounded-full absolute top-[-30] right-[-30]"></div>
+            <Decoration />
         </>
       )}
     </div>
--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,12 +1,37 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function top() {
     return (
-        <main className="h-svh ">
+        <div className="h-svh flex flex-col justify-between">
             <Header />
+            <main>
+                    <section className="flex items-center justify-center bg-[#48A5BC] h-[318px]">
+                        <div className="mr-[30px]">
+                            <p className="text-[#fff]">今の生活を流づけると...</p>
+                            <h2></h2>
+                        </div>
+                        <Image src="character.svg" alt="キャラクター" width={122} height={218} />
+                    </section>
+                <section className="flex flex-col items-center">
+                    <section className="py-[20px]">
+                        <div className="flex justify-center gap-[60px] text-[#fff] font-bold pb-[19px] border-b-[#AFB2B2] border-b-[2px]">
+                            <button className="bg-[#48A5BC] w-[140px] p-[10px] rounded-[40px]">今日のポイント</button>
+                            <button className="bg-[#48A5BC] w-[140px] p-[10px] rounded-[40px]">今週の目標</button>
+                        </div>
+                        <div className="px-[35px] py-[20px]">
+                            <li></li>
+                        </div>
+                    </section>
+                    <Link href="/log">
+                        <button className="text-[#fff] bg-[#F1C168] w-[140px] h-[46px] text-[18px] font-bold rounded-[8px] ">記録</button>
+                    </Link>
+                </section>
+            </main>
             <Footer />
-        </main>
+        </div>
     )
 }
