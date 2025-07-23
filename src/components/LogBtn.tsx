b9835f0b7c0ca6d2359629c253be9de3a47ba433
kanayakaisei <kanayakaisei0903@gmail.com>
2025-07-23 11:41:03 +0900

    リンク先設定

--- src/components/LogBtn.tsx
+++ src/components/LogBtn.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
+
 export default function LogBtn(){
     return(
         <div className="flex justify-center">
-            <button className="bg-[#F1C168] text-[#fff] text-[18px] font-bold rounded-[5px] px-[52px] py-[13px] shadow-[1px_1px_3px_0_rgba(0,0,0,0.25)]">
-                記録
-            </button>
+            <Link href="/record">
+                <button className="bg-[#F1C168] text-[#fff] text-[18px] font-bold rounded-[5px] px-[52px] py-[13px] shadow-[1px_1px_3px_0_rgba(0,0,0,0.25)]">
+                    記録
+                </button>
+            </Link>
         </div>
     )
 }
